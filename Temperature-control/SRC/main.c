5c755a94ad1998c4248837211797ff18b4603383
ehsan hoseini <ehsan.tc.evol@gmail.com>
2021-08-03 16:25:44 +0430

    .

--- Temperature-control/SRC/main.c
+++ Temperature-control/SRC/main.c
@@ -1,20 +1,18 @@
 // THIS CODE IS WRITTEN BY @ehsan1381
 
 // PREPROCESSOR
-#define atmega16
-#define _LCD_PORT                   PORTB
+#define atmega16a
 #define _DRIVER_PORT                DDRD
 #define _MAX_TEMP                   40
 #define _ALERT_TEMP                 45
-#define _MAX_MOTOR_FREQ             40000
-#define _MOTOR_FREQ_STEP            5000
-#define _NUMBER_OF_MOTORS           1
+#define _MAX_MOTOR_FREQ             501
+#define _MOTOR_FREQ_STEP            10
 #define _COOL_TEMP_HIGH             25
 #define _NORM_TEMP_LOW              25
 #define _NORM_TEMP_HIGH             35
 #define _WARM_TEMP_LOW              35
 #define _WARM_TEMP_HIGH             40
-#define _SENSOR_TEMP_EQ(ADC_VALUE)  (ADC_VALUE / 10)
+#define _SENSOR_TEMP_EQ(ADC_VALUE)  (ADC_VALUE / 2.05)
 
 
 // INCLUDE HEADERS
@@ -24,33 +22,36 @@
 #include <stdio.h>
 
 // FINCTION PROTOTYPES
-char DEFINE_TEMP_STATE(int);
-void WRITE_LCD(int, int);
+void DEFINE_TEMP_STATE(int);
+void WRITE_LCD(int, int, char);
 void MOTOR_DRIVE_MAN();
-void MOTOR_DRIVE_AUT(char);
+void MOTOR_DRIVE_AUT(char, int);
 
 // VARIABLES
 char LINE0_STR[18];
 char LINE1_STR[18];
 int MOTOR_GEAR = 0;
 char TEMP_STATE = 'N';
-char CONTROL_TYPE = 1;
-int TEMP = 0;
+char DRIVE_STATE; 
+
+int TEMP;
 
 // TEMP STATE FUNC
-char DEFINE_TEMP_STATE(int TEMP) {
-    if (TEMP < _COOL_TEMP_HIGH) return 'C';
-    if (_NORM_TEMP_LOW <= TEMP < _NORM_TEMP_HIGH) return 'N';
-    if (_WARM_TEMP_LOW <= TEMP < _WARM_TEMP_HIGH) return 'W';
-    if (_MAX_TEMP <= TEMP <_ALERT_TEMP) return 'M';
-    if (TEMP > _ALERT_TEMP) return 'A';
+void DEFINE_TEMP_STATE(int TEMP) {
+
+    if (TEMP < _COOL_TEMP_HIGH) TEMP_STATE =  'C';
+    if (_NORM_TEMP_LOW <= TEMP && TEMP < _NORM_TEMP_HIGH) TEMP_STATE =  'N';
+    if (_WARM_TEMP_LOW <= TEMP && TEMP < _WARM_TEMP_HIGH) TEMP_STATE =  'W';
+    if (_MAX_TEMP <= TEMP && TEMP <_ALERT_TEMP) TEMP_STATE =  'M';
+    if (TEMP > _ALERT_TEMP) TEMP_STATE =  'A';     
+
 }
 
 // WRITE LCD FUNC
-void WRITE_LCD(int temp, int motor_gear) {
-    lcd_clear();
-    sprintf(LINE0_STR, "TEMP : %d", temp);
-    sprintf(LINE1_STR, "MOTOR : %d", motor_gear);
+void WRITE_LCD(int temp, int motor_freq, char drive_state) {
+    lcd_clear();   
+    sprintf(LINE0_STR, "TEMP : %d C %c", temp, TEMP_STATE);
+    sprintf(LINE1_STR, "MOTOR : %d Hz %c", motor_freq, drive_state);
     lcd_puts(LINE0_STR);
     lcd_gotoxy(0, 1);
     lcd_puts(LINE1_STR);
@@ -59,17 +60,17 @@ void WRITE_LCD(int temp, int motor_gear) {
 
 // MOTOR DRIVING MAN FUNC
 void MOTOR_DRIVE_MAN() {
-    if (PIND.7 == 0) {
-        MOTOR_GEAR++;
+    DRIVE_STATE = 'M';
+    if (PIND.7 == 1) {
+        MOTOR_GEAR += 5;
         if ((MOTOR_GEAR * _MOTOR_FREQ_STEP) >= _MAX_MOTOR_FREQ) MOTOR_GEAR = 0;
     }
 
     if (PIND.6 == 0) {
         PORTD.0 = 0;
         PORTD.1 = 1;
-    }
-
-    else {
+    } 
+    if (PIND.6 == 1) {
         PORTD.0 = 1;
         PORTD.1 = 0;
     }
@@ -77,28 +78,43 @@ void MOTOR_DRIVE_MAN() {
 }
 
 // MOTOR DRIVING AUT FUNC
-void MOTOR_DRIVE_AUT(char temp_state) {
-    switch (temp_state) {
+void MOTOR_DRIVE_AUT(char temp_state, int temp) {
+    DRIVE_STATE = 'A';
+    switch (temp_state) {      
+
     case 'C':
-        MOTOR_GEAR = 0;
+        MOTOR_GEAR = 5;
         break;
 
     case 'N':
-        MOTOR_GEAR = 1;
+        MOTOR_GEAR = temp / 2.5 + 10;
         break;
 
     case 'W':
-        MOTOR_GEAR = 4;
+        MOTOR_GEAR = temp / 2.5 + 25;
         break;
 
     case 'M':
-        MOTOR_GEAR = 6;
+        MOTOR_GEAR = temp / 2.5 + 20;
         break;
 
     case 'A':
-        MOTOR_GEAR = 8;
-        break;
-    }
+        MOTOR_GEAR = 50; 
+        
+        break;     
+        
+    }     
+    
+    if (MOTOR_GEAR > 50) MOTOR_GEAR = 50;
+    
+        if (PIND.6 == 0) {
+        PORTD.0 = 0;
+        PORTD.1 = 1;
+    } 
+    if (PIND.6 == 1) {
+        PORTD.0 = 1;
+        PORTD.1 = 0;  
+        }
 }
 
 // LCD CONFIGURATION
@@ -127,26 +143,30 @@ unsigned int read_adc(unsigned char adc_input)
 void main(void)
 {
     
-_DRIVER_PORT = 0x33;
+    _DRIVER_PORT = 0x33;
 
-TCCR1A = (1 << COM1A1) | (1 << COM1B1) | (1 << WGM11);
-TCCR1B = (1 << CS10);
+    TCCR1A = (1 << COM1A1) | (1 << COM1B1) | (1 << WGM11);
+    TCCR1B = (1 << CS10);
 
 
-ADMUX = ADC_VREF_TYPE;
-ADCSRA = (1 << ADEN) | (0 << ADSC) | (0 << ADATE) | (0 << ADIF) | (0 << ADIE) | (1 << ADPS2) | (0 << ADPS1) | (0 << ADPS0);
+    ADMUX = ADC_VREF_TYPE;
+    ADCSRA = (1 << ADEN) | (0 << ADSC) | (0 << ADATE) | (0 << ADIF) | (0 << ADIE) | (1 << ADPS2) | (0 << ADPS1) | (0 << ADPS0);
 
-lcd_init(16);
+    lcd_init(16);
 
     while (1)
     {
-        CONTROL_TYPE = PINA.2;
-        TEMP = read_adc(0);
-        TEMP_STATE = DEFINE_TEMP_STATE(TEMP);
-        if (CONTROL_TYPE) MOTOR_DRIVE_AUT(TEMP_STATE);
-        if (!CONTROL_TYPE) MOTOR_DRIVE_MAN();
-        OCR1A = MOTOR_GEAR * _MOTOR_FREQ_STEP;
-        delay_ms(100);
+        TEMP = _SENSOR_TEMP_EQ(read_adc(0));
+        DEFINE_TEMP_STATE(TEMP);
+        
+        
+        if(PINA.1 == 1) MOTOR_DRIVE_AUT(TEMP_STATE, TEMP);
+        if(PINA.1 == 0) MOTOR_DRIVE_MAN();
+
+        WRITE_LCD(TEMP, MOTOR_GEAR * _MOTOR_FREQ_STEP, DRIVE_STATE);
+                                       
+        OCR1A = (MOTOR_GEAR * _MOTOR_FREQ_STEP);
+        delay_ms(10);
 
     }
 }
